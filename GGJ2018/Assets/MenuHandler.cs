f75e22e895f293c7c936dfd0dc5125aa0c8ad0ef
Daniel <dnewman52@gmail.com>
2018-01-27 15:51:30 +1100

    MainMenu Setup with Audio implemented

---
+++ GGJ2018/Assets/MenuHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+
+    public GameObject mainPanel;
+    public GameObject optionsPanel;
+    public AudioMixer mixer;
+
+    Slider musicSlider;
+    Slider soundSlider;
+
+    private void Awake()
+    {
+        musicSlider = GameObject.Find("Music Volume").GetComponent<Slider>();
+        soundSlider = GameObject.Find("Sound Effects Volume").GetComponent<Slider>();
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void OnClickStart(string lvl)
+    {
+        //Setup as basic lvl loading
+        SceneManager.LoadScene(lvl);
+    }
+
+    public void OnClickOptions()
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void OnClickBack()
+    {
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeMusicVolume()
+    {
+        float val = musicSlider.value;
+        mixer.SetFloat("MusicVolume", val);
+    }
+
+    public void ChangeSEVolume()
+    {
+        float val = soundSlider.value;
+        mixer.SetFloat("SoundEffectsVolume", val);
+    }
+}
