67dcfb2f764dfc6122b3e6eef09c67ea5b415d10
Dan-PC <d.szabo87@gmail.com>
2018-02-08 22:43:08 +1100

    Fixed minor issue with touch screen tap position being the average of touches

--- GGJ2018/Assets/Scripts/Core/Input/PlanetClicker.cs
+++ GGJ2018/Assets/Scripts/Core/Input/PlanetClicker.cs
@@ -7,25 +7,45 @@ public class PlanetClicker : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        bool clicked = false;
+        Vector2 mousePos = Vector2.zero;
+
+        if (Input.touchCount > 0)
         {
-            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            LayerMask layers = Helpers.GetMaskInt(Helpers.Layer.Background) | Helpers.GetMaskInt(Helpers.Layer.Planet) | 1 << LayerMask.NameToLayer("UI");
-            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector3.forward, layers);
-            if (hit.collider != null)
+            clicked = true;
+            mousePos = Input.touches[0].position;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            clicked = true;
+            mousePos = Input.mousePosition;
+        }
+
+
+        if (clicked)
+        {
+            CheckPlanetClicked(mousePos);
+        }
+    }
+
+    private void CheckPlanetClicked(Vector2 mousePos)
+    {
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        LayerMask layers = Helpers.GetMaskInt(Helpers.Layer.Background) | Helpers.GetMaskInt(Helpers.Layer.Planet) | 1 << LayerMask.NameToLayer("UI");
+        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector3.forward, layers);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.layer == Helpers.GetMask(Helpers.Layer.Planet))
             {
-                if (hit.collider.gameObject.layer == Helpers.GetMask(Helpers.Layer.Planet))
-                {
-                    GameUI.Instance.LogUI.SetLogFilter(hit.collider.GetComponent<Planet>());
-                }
-                else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("UI"))
-                {
-                    return;
-                }
-                else
-                {
-                    GameUI.Instance.LogUI.ClearFilter();
-                }
+                GameUI.Instance.LogUI.SetLogFilter(hit.collider.GetComponent<Planet>());
+            }
+            else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("UI"))
+            {
+                return;
+            }
+            else
+            {
+                GameUI.Instance.LogUI.ClearFilter();
             }
         }
     }
